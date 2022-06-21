a1864be48da555fc99506f33bae0dee88a9cddc9
laeeq <laeeqhaq@gmail.com>
2022-06-21 21:19:56 +0600

    commit

---
+++ src/components/Button-dark.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ButtonDark = (props: any) => {
+  return (
+    <a
+      href={props.link}
+      className={`transition-colors duration-300 bg-primaryPurple hover:bg-primaryPurpleDarker w-48 mx-8 py-4 text-center hover:cursor-pointer rounded-full ${props.classes}`}
+    >
+      <h6 className={`text-white font-bold ${props.textClasses}`}>
+        {props.text}
+      </h6>
+    </a>
+  );
+};
+
+export default ButtonDark;

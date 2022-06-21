a9f359c649195e4c6dd83ca2178256c6399e5bcc
laeeq <laeeqhaq@gmail.com>
2022-06-21 22:09:01 +0600

    commit

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,11 +1,19 @@
-import React from "react"
+import React from "react";
 
 const HomePage = () => {
   return (
     <div className="w-screen flex col text-center">
-      <h1>Hello World</h1>
+      <section className="hero">
+        <article>
+          <h1>Generate unique SVG design assets</h1>
+          <p className="">
+            Haikei is a web app to generate unique SVG shapes, backgrounds, and
+            patterns – ready to use with your design tools and workflow.
+          </p>
+        </article>
+      </section>
     </div>
-  )
-}
+  );
+};
 
 export default HomePage;
